8ec07a10578f0b2aa7075f7786f858c368ea542d
tier3guy <avinashgupta.works@gmail.com>
2025-10-31 19:56:23 +0530

    refactor: remove unused imports in MobileTableOfContents component

--- components/mobile-toc.tsx
+++ components/mobile-toc.tsx
@@ -2,9 +2,8 @@
 
 import React from "react";
 import { List } from "lucide-react";
-import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerBody, DrawerFooter } from "@/components/ui/drawer";
 import { TableOfContents } from "@/components/table-of-contents";
-import { PromoContent } from "@/components/promo-content";
+import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerBody } from "@/components/ui/drawer";
 
 export function MobileTableOfContents() {
   return (
